9ea7b067b078a098c8a92f091759349060c62d01
Al Capitan <60216981+alcapitan@users.noreply.github.com>
2021-04-24 19:18:25 +0200

    Mise à jour du répertoire

--- program.js
+++ program.js
@@ -1,8 +1,14 @@
 function encrypt()
 {
-     entre = window.prompt("Entrée : ");
+     console.clear();
+     entre = window.prompt("Message à crypter : ");
      cle = window.prompt("Clé : ");
-     console.log("Entrée : " + entre);
+     if (entre.length != 6 || cle.length !=6)
+     {
+          console.log("Erreur utilisateur");
+          return;
+     }
+     console.log("Message à crypter : " + entre);
      console.log("Clé : " + cle);
 
      let longueur = entre.length;
@@ -13,6 +19,18 @@ function encrypt()
           let separateEntre = entre[compteur];
           let separateCle = cle[compteur];
           compteur++;
+          if (separateEntre != 0 || separateEntre !=1)
+          {
+               console.clear();
+               console.log("Erreur utilisateur");
+               return;
+          }
+          if (separateCle != 0 || separateCle !=1)
+          {
+               console.clear();
+               console.log("Erreur utilisateur");
+               return;
+          }
           if (separateEntre == separateCle)
           {
                produit += "1";
@@ -22,7 +40,50 @@ function encrypt()
                produit += "0";
           }
      }
-     console.log("Produit : " + produit);
+     console.log("Message crypté : " + produit);
 }
 
-encrypt();
+function decrypt()
+{
+     console.clear();
+     entre = window.prompt("Message à décrypter : ");
+     cle = window.prompt("Clé : ");
+     if (entre.length != 6 || cle.length !=6)
+     {
+          console.log("Erreur utilisateur");
+          return;
+     }
+     console.log("Message à décrypter : " + entre);
+     console.log("Clé : " + cle);
+
+     let longueur = entre.length;
+     let compteur = 0;
+     let produit = "";
+     while (compteur != longueur)
+     {
+          let separateEntre = entre[compteur];
+          let separateCle = cle[compteur];
+          compteur++;
+          if (separateEntre != 0 || separateEntre !=1)
+          {
+               console.clear();
+               console.log("Erreur utilisateur");
+               return;
+          }
+          if (separateCle != 0 || separateCle !=1)
+          {
+               console.clear();
+               console.log("Erreur utilisateur");
+               return;
+          }
+          if (separateEntre == separateCle)
+          {
+               produit += "0";
+          }
+          else
+          {
+               produit += "1";
+          }
+     }
+     console.log("Message décrypté : " + produit);
+}
